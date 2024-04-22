c3a69e28e023e2d5f5c144c28509a07787b98418
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-04-22 15:12:58 -0300

    Menu updated
    Verificação para terminar o loop.
    Método para mostrar posteriormente as informações do calculo.

--- assets/conversion/src/main/Main.java
+++ assets/conversion/src/main/Main.java
@@ -1,42 +1,56 @@
 package main;
 
+import java.io.Console;
+import java.lang.ref.Cleaner;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void showQuantity() {
+        System.out.println("How much? ");
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
 
             System.out.println("Welcome! To the currency converter!");
             System.out.println("-----------------------------------");
             System.out.println("""
-                        [1] Dollar -> Argentine currency
-                        [2] Dollar -> Colombian currency
-                        [3] Dollar -> Real
-                        [4] Real -> Dollar
-                        [5] Argentine currency -> Dollar
-                        [6] Colombian currency -> Dollar
-                        [7] Exit
+                    [1] Dollar -> Argentine currency
+                    [2] Dollar -> Colombian currency
+                    [3] Dollar -> Real
+                    [4] Real -> Dollar
+                    [5] Argentine currency -> Dollar
+                    [6] Colombian currency -> Dollar
+                    [7] Exit
                     """);
 
             byte choice = scanner.nextByte();
 
+            if (choice == 7) {
+                break;
+            }
+
             switch (choice) {
                 case 1:
-
+                    showQuantity();
                     break;
                 case 2:
+                    showQuantity();
                     break;
                 case 3:
+                    showQuantity();
                     break;
                 case 4:
+                    showQuantity();
                     break;
                 case 5:
+                    showQuantity();
                     break;
                 case 6:
-                    break;
-                case 7:
+                    showQuantity();
                     break;
                 default:
                     System.out.println("Invalid option");
