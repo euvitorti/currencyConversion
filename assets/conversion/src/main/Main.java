2a73f9af4f6485121a33ab059e663f360a0d5530
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-04-25 16:49:24 -0300

    Organização do main
    Tratamento de erro para entradas incorretas.
    Criando um objeto da classe connection, e passando os valores.

--- assets/conversion/src/main/Main.java
+++ assets/conversion/src/main/Main.java
@@ -2,61 +2,86 @@ package main;
 
 import api.Connection;
 
-import java.io.Console;
-import java.lang.ref.Cleaner;
+import java.util.IllegalFormatException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void conectar(String one, String two) {
-        Connection connection = new Connection(one, two);
+    public static void menu() {
+        System.out.println("Welcome! To the currency converter!");
+        System.out.println("-----------------------------------");
+        System.out.println("""
+                        [1] Dollar -> Argentine currency
+                        [2] Dollar -> Colombian currency
+                        [3] Dollar -> Real
+                        [4] EUR -> Dollar
+                        [5] EUR -> Argentine currency
+                        [6] EUR -> BRL
+                        [7] Exit
+                        """);
+    }
+
+    public static void connect(String one, String two) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("How much? ");
+            double baseValue = scanner.nextDouble();
+
+            Connection connection = new Connection(one, two, baseValue);
+        } catch (Exception e) {
+            System.out.println("Try again.");
+            System.out.println("Only numbers!");
+        }
+
     }
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        byte choice = 0;
+
         while (true) {
 
-            System.out.println("Welcome! To the currency converter!");
-            System.out.println("-----------------------------------");
-            System.out.println("""
-                    [1] Dollar -> Argentine currency
-                    [2] Dollar -> Colombian currency
-                    [3] Dollar -> Real
-                    [4] EUR -> Dollar
-                    [5] EUR -> Argentine currency
-                    [6] EUR -> BRL
-                    [7] Exit
-                    """);
-
-            byte choice = scanner.nextByte();
+            try {
+                menu();
+                choice = scanner.nextByte();
+            }
+            catch (Exception e) {
+                System.out.println("Try again.");
+                System.out.println("Only numbers!");
+            }
 
             if (choice == 7) {
+                System.out.println("Program successfully completed");
                 break;
             }
 
-            switch (choice) {
+            switch (choice)
+            {
                 case 1:
-                    conectar("USD", "ARS");
+                    connect("USD", "ARS");
                     break;
                 case 2:
-                    conectar("USD", "COP");
+                    connect("USD", "COP");
                     break;
                 case 3:
-                    conectar("USD", "BRL");
+                    connect("USD", "BRL");
                     break;
                 case 4:
-                    conectar("EUR", "USD");
+                    connect("EUR", "USD");
                     break;
                 case 5:
-                    conectar("EUR", "ARS ");
+                    connect("EUR", "ARS");
                     break;
                 case 6:
-                    conectar("EUR", "BRL");
+                    connect("EUR", "BRL");
                     break;
                 default:
                     System.out.println("Invalid option");
                     break;
             }
         }
+
     }
 }
